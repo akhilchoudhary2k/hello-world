e8947052e3f0c4ee82616741b914351e88166aab
akhilchoudhary2k <akhil.choudhary2000@gmail.com>
2020-01-18 15:56:25 +0530

    added name prompt to myclass.java

--- myclass.java
+++ myclass.java
@@ -1,8 +1,14 @@
 //java file
+import java.util.Scanner;
+
 class myclass
 {
 	public static void main(String args[])
 	{
 		System.out.println("Hello there :)");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("What is your name : ");
+		String name = sc.nextLine();
+		System.out.println("Hello " + name +" this is a basic java file");
 	}
 }
